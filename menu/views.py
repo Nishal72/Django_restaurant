986e05067ccaff4a917a29e2d8517b97c7269227
Veer <veerbowanee@gmial.com>
2025-03-31 12:35:52 +0400

    Initial commit

---
+++ menu/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from menu.models import Item
+# Create your views here.
+
+def home(request):
+    Items=Item.objects.all()
+    return render(request, 'menu/home.html', {'items':Items})
+
+def about(request):
+    return HttpResponse("This is the about page.")
